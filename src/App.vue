2f5b66a40520865ecdc7958ce91ff6086fcc9d44
djvaij <djvaij@gmail.com>
2021-02-04 23:12:34 +0200

    Header component beta.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <Header />
+  <div class="viewport">
+    <div class="global-container">
+      <Header />
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,5 +11,25 @@ import Header from './components/Header.vue'
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
 
+html,
+body {
+  margin: 0;
+  // background: #eeeeee;
+  background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+}
+
+.viewport {
+  // background: #eeeeee;
+  background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+}
+
+.global-container {
+  margin: auto;
+  width: 100%;
+  max-width: 920px;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
-  <header>
-    s
+  <header class="header">
+    <input type="text" class="header__input" placeholder="Пошук...">
+    <button class="header__button">+</button>
   </header>
 </template>
 
@@ -15,6 +16,59 @@ const state = reactive({ count: 0 })
 </script>
 
 <style scoped lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+  padding: 15px 20px;
+  background: #f7f7f7;
+  border-radius: 5px;
+}
+
+.header {
+  &__input {
+    width: 100%;
+    padding: 10px 15px;
+    border: 1px solid #acacac;
+    border-radius: 5px;
+    font-size: 20px;
+    color: #42b983;
+    
+    &:focus {
+      border: 1px solid #42b983;
+      outline: none;
+    }
+
+    &::placeholder {
+      color: rgba(#acacac, 0.7);
+    }
+  }
+
+  &__button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    max-width: 45.2px;
+    max-height: 45.2px;
+    margin-left: 25px;
+    padding: 10px 15px;
+    border: 1px solid #acacac;
+    border-radius: 5px;
+    color: #acacac;
+    font-size: 32px;
+    font-weight: bold;
+    transition: all 0.3s ease-in-out;
+    background: white;
+    cursor: pointer;
+
+    &:hover {
+      border: 1px solid #42b983;
+      color: #42b983;
+    }
+  }
+}
+
 a {
   color: #42b983;
 }
