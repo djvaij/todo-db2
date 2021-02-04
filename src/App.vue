96fb2d5f4c070d91d3b3efa2b7210e3eb284c893
djvaij <djvaij@gmail.com>
2021-02-04 22:28:20 +0200

    Replace component 'HelloWorld.vue' to 'Header.vue'.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Header />
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    s
+  </header>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped lang="scss">
+a {
+  color: #42b983;
+}
+</style>
